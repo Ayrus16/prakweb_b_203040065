5acb19251515d7e0942e09f3de7a02e481d647ba
Ayrus16 <suryap.sp83@gmail.com>
2022-10-07 20:34:47 +0700

    Video #8 - Overriding

--- oop/overriding.php
+++ oop/overriding.php
@@ -4,17 +4,13 @@ class Produk{
     public  $judul,
             $penulis,
             $penerbit,
-            $harga,
-            $jmlHalaman,
-            $waktuMain;
+            $harga;
 
-     public function __construct($judul= "judul", $penulis="penulis", $penerbit= "perbit",$harga= 0,$jmlHalaman = 0,$waktuMain=0){
+     public function __construct($judul= "judul", $penulis="penulis", $penerbit= "perbit",$harga= 0){
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
-        $this->jmlHalaman = $jmlHalaman;
-        $this->waktuMain = $waktuMain;
 
     }
     public function getLabel(){
@@ -23,13 +19,23 @@ class Produk{
 
     public function getInfoProduk(){
         $str = " {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-
+        return $str;
     }
 
     
 }
 
 class Komik extends Produk{
+    public $jmlHalaman;
+
+    public function __construct($judul= "judul", $penulis="penulis", $penerbit= "perbit",$harga= 0,$jmlHalaman=0)
+    {
+        parent::__construct($judul, $penulis, $penerbit,$harga);
+
+        $this->jmlHalaman = $jmlHalaman;
+
+    }
+
     public function getInfoProduk()
     {
         $str = "Komik : ".parent::getInfoProduk()."  - {$this->jmlHalaman} Halaman.";
@@ -38,6 +44,17 @@ class Komik extends Produk{
 }
 
 class Game extends Produk{
+    
+    public $waktuMain;
+
+    public function __construct($judul= "judul", $penulis="penulis", $penerbit= "perbit",$harga= 0,$waktuMain=0)
+    {
+        parent::__construct($judul, $penulis, $penerbit,$harga);
+
+        $this->waktuMain = $waktuMain;
+
+    }
+
     public function getInfoProduk()
     {
         $str = "Game : ".parent::getInfoProduk()."  - {$this->waktuMain} jam.";
@@ -55,9 +72,9 @@ class CetakInfoProduk{
     }
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100,0,);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000,0,50);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000,50);
 
 
 
