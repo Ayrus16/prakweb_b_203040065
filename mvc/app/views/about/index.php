8501d006c6b6d53af57f9354e2828e81693b91a6
Ayrus16 <suryap.sp83@gmail.com>
2022-10-14 20:35:17 +0700

    Selesaikan Video#6 - Asset

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL;?>/img/nophoto.jpg" alt="" width = "200">
     <p>Halo, nama saya <?=$data['nama']?> saya berumur <?=$data['umur']?>, merupakan <?=$data['pekerjaan']?></p>
+    </div>
--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Home</title>
-</head>
-<body>
-    <h1>Selamat datang di website saya</h1>
-</body>
-</html>
+<div class="container">
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SELAMAT DATANG</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
