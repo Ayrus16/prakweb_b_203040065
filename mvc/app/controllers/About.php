7ce3705e8e9648692cafad902e04a2852e5c003f
Ayrus16 <suryap.sp83@gmail.com>
2022-10-14 19:38:26 +0700

    Selesaikan Video#5 - View

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,10 +1,21 @@
 <?php
 
-class About{
-    public function index($nama = 'Surya',$pekerjaan='Mahasiwa' ){
-        echo"Hallo, nama saya $nama adalah seorang $pekerjaan";
+class About extends Controller{
+    public function index($nama = 'Surya',$pekerjaan='Mahasiwa' ,$umur = 20){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        
+        $this->view('template/header', $data);
+        $this->view('about/index', $data);
+        $this->view('template/footer');
+        
     }
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('template/header', $data);
+        $this->view('about/page');
+        $this->view('template/footer');
     }
 }
---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About Me</h1>
+    <p>Halo, nama saya <?=$data['nama']?> saya berumur <?=$data['umur']?>, merupakan <?=$data['pekerjaan']?></p>
